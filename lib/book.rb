a719752f7b8b40719987238ba031374f6c863b00
the-alchemist-gh <malikbanks01@gmail.com>
2022-08-30 10:27:40 +0000

    Completed lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
 
+class Book
+    attr_accessor :title,:author,:page_count,:genre
 
+    def initialize(title="And Then There Were None")
+        @title = title
+    end
+
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+end
